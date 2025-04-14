81682b2332945c0c26f1797e3dfc73876ffd54c3
Ayubxon <ayubxont19@gmail.com>
2025-04-14 13:15:02 +0300

    xatolik tuzatildi

--- home/signals.py
+++ home/signals.py
@@ -2,27 +2,22 @@ from django.db.models.signals import post_delete, post_save, pre_delete
 from django.dispatch import receiver
 from .models import Cost_Model, Book_Model, Income_Model
 
-@receiver([post_save , post_delete], sender=Cost_Model)
-@receiver([post_save , post_delete], sender=Income_Model)
+@receiver([post_save, post_delete], sender=Cost_Model)
+@receiver([post_save, post_delete], sender=Income_Model)
 def signal_quantity(sender, instance, **kwargs):
+    if sender == Cost_Model:
+        book = instance.name  # ForeignKey(Book_Model)
+    elif sender == Income_Model:
+        book = instance.sold_book  # ForeignKey(Book_Model)
+    else:
+        return
 
-    try:
-        Cost_list = Cost_Model.objects.filter(name__name=instance.name)
-        book = Book_Model.objects.get(id=instance.name.id)
-        Income_list = Income_Model.objects.filter(sold_book__name=instance.name)
-    except:
-        Cost_list = Cost_Model.objects.filter(name__name=instance.sold_book)
-        book = Book_Model.objects.get(name=instance.sold_book)
-        Income_list = Income_Model.objects.filter(sold_book__name=instance.sold_book)
+    # Umumiy hisoblash
+    cost_list = Cost_Model.objects.filter(name=book, is_deleted=False)
+    income_list = Income_Model.objects.filter(sold_book=book,  is_deleted=False)
 
-        cost_quantity = 0
-        sold_book_quantity = 0 
+    cost_quantity = sum(item.quantity for item in cost_list)
+    sold_quantity = sum(item.quantity for item in income_list)
 
-        for i in Cost_list:
-            cost_quantity += i.quantity
-
-        for i in Income_list:
-            sold_book_quantity += i.quantity
-
-        book.quantity = cost_quantity - sold_book_quantity
-        book.save()
+    book.quantity = cost_quantity - sold_quantity
+    book.save()
